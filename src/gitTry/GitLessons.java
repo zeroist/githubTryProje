75af3e2526e4593d144bb86fe1883786c62e90a1
Yusuf  OLGUN <abd_yusuf@yahoo.com>
2022-05-22 19:36:01 +0300

    first change

--- src/gitTry/GitLessons.java
+++ src/gitTry/GitLessons.java
@@ -3,5 +3,6 @@ package gitTry;
 public class GitLessons {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("thats it");
     }
 }
